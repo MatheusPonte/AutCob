e3a3c807ed11b191734659b389e7550d3230fafa
matheusponte <matheuspontes894@outlook.com>
2024-10-08 11:35:01 -0300

    Fix: Pull type CPF/CNPJ to String

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import pandas
 arquivo_excel = 'BASE DE DEVEDORES EXTRAJUDICIAL.xlsx'
 
 # Carregar a planilha em um DataFrame
-df = pandas.read_excel(arquivo_excel)
+df = pandas.read_excel(arquivo_excel, dtype={'CPF/CNPJ': str})
 
 # Acessar a coluna "nome" e armazenar em um array (lista)
 nomes = df['CPF/CNPJ'].tolist()
